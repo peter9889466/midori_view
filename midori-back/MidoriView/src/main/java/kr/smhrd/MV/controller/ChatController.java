f779fd8fd9e03e21b7ae0464a9075049879142be
김승혁 <peter9889466@gmail.com>
2025-07-02 14:28:52 +0900

    feat: connect front

--- midori-back/MidoriView/src/main/java/kr/smhrd/MV/controller/ChatController.java
+++ midori-back/MidoriView/src/main/java/kr/smhrd/MV/controller/ChatController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chat")
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ChatController {
 
     @Autowired

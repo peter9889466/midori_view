c59e4668b0044d63df965b4a6cab3021ffc97ed6
김승혁 <peter9889466@gmail.com>
2025-07-07 10:10:47 +0900

    fix: CORS

--- midori-back/MidoriView/src/main/java/kr/smhrd/MV/config/WebConfig.java
+++ midori-back/MidoriView/src/main/java/kr/smhrd/MV/config/WebConfig.java
@@ -2,28 +2,21 @@ package kr.smhrd.MV.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/MV/api/**")
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
                 .allowedOrigins(
-                        "http://49.50.134.156:3000",
-                        "http://49.50.134.156:5173",
-                        "http://49.50.134.156:5174",
-                        "http://localhost:3000",
-                        "http://localhost:5173",
-                        "http://localhost:5174"
+                    "http://49.50.134.156:5173",
+                    "http://49.50.134.156:5174",
+                    "http://49.50.134.156:3000"
                 )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowCredentials(true);
     }
 }
